df9d4a88291c5495f25f54278d82dd9b817ddc0b
faris-studio <farissyaifulloh123@gmail.com>
2023-06-20 19:12:28 +0700

    update

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Faris Syaifulloh" 
+const studentId = "FE5551943" // TODO: replace this
+
+export { url, studentName, studentId }
